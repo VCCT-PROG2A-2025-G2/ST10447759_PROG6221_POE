8fa5f234e062d5cf6d143c84ddc01d762854c1d9
Jeron <jeronokkers7@gmail.com>
2025-05-26 18:56:36 +0200

    add unit test

---
+++ CyberSecurityBot.Tests/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CyberSecurityBot.Tests
+{
+    [TestClass]
+    public class CyberBotTests
+    {
+        [TestMethod]
+        public void RespondToKeyword_ShouldReturnPasswordTip_WhenInputContainsPassword()
+        {
+            // Arrange
+            var bot = new CyberBot();
+            string input = "Tell me about password safety";
+
+            // Act
+            string response = bot.RespondToUser(input);
+
+            // Assert
+            Assert.IsTrue(response.Contains("password", StringComparison.OrdinalIgnoreCase));
+        }
+    }
+
+}
